0483f9ae357ce1a35fd43da148290963dff3ffec
Ronagar <rnginformatica@uma.es>
2023-11-22 22:26:08 +0100

    V2.1. Ahora crea 3 QR por alumno

--- createQR.py
+++ createQR.py
@@ -7,24 +7,30 @@ with open('alumnos.csv') as file:
     reader = csv.DictReader(file)
     headers = [header.strip() for header in reader.fieldnames]
     for row in reader:
-        #print(row.get(headers[0]),row.get(headers[2]))
-        #print(row['nombre'], row['ID'])
         student=row.get(headers[1])+row.get(headers[0])
-        QRcode = qrcode.QRCode(error_correction=qrcode.constants.ERROR_CORRECT_H)
-
-        #Feed the QR Code data
-        data = row.get(headers[2])
-        QRcode.add_data(data)
-        QRcode.make
+        
 
         #Creation of the path if neccessary
         Path('./outputs/'+student).mkdir(parents=True, exist_ok=True)
-        #Creation of the image
-        QRimg = QRcode.make_image()
         
-        #Save the image .png
-        path = './outputs/'+student+'/'+data+'.png'
-        QRimg.save(path)
+
+        for letter in ('a','b','c'):
+            print(letter)
+            QRcode = qrcode.QRCode(error_correction=qrcode.constants.ERROR_CORRECT_H)
+            #extract the student id from the dataSet
+            data = row.get(headers[2])
+            #Feed the QR Code data
+            data = data+'_'+letter
+            print(data)
+            QRcode.add_data(data)
+            QRcode.make
+
+            #Creation of the image
+            QRimg = QRcode.make_image()
+        
+            #Save the image .png
+            path = './outputs/'+student+'/'+data+'.png'
+            QRimg.save(path)
      
 file.close()
 
