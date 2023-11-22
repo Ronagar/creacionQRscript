fb72e2f5ae7c5e023e52048a6f2ba89cd7d971b7
Ronagar <rnginformatica@uma.es>
2023-11-22 22:02:05 +0100

    V2. Genera QRs en funcion de un csv

--- createQR.py
+++ createQR.py
@@ -1,16 +1,33 @@
+from pathlib import Path
 import qrcode
 from PIL import Image
+import csv 
 
-owner='miguel'
-QRcode = qrcode.QRCode(error_correction=qrcode.constants.ERROR_CORRECT_H)
+with open('alumnos.csv') as file:
+    reader = csv.DictReader(file)
+    headers = [header.strip() for header in reader.fieldnames]
+    for row in reader:
+        #print(row.get(headers[0]),row.get(headers[2]))
+        #print(row['nombre'], row['ID'])
+        student=row.get(headers[1])+row.get(headers[0])
+        QRcode = qrcode.QRCode(error_correction=qrcode.constants.ERROR_CORRECT_H)
 
-#Feed the QR Code data
-datos='Hola buenas tardes :D'
-QRcode.add_data(datos)
-QRcode.make
+        #Feed the QR Code data
+        data = row.get(headers[2])
+        QRcode.add_data(data)
+        QRcode.make
 
-#Creation of the image
-QRimg = QRcode.make_image()
-path = './'+owner+'.png'
-QRimg.save(path)
+        #Creation of the path if neccessary
+        Path('./outputs/'+student).mkdir(parents=True, exist_ok=True)
+        #Creation of the image
+        QRimg = QRcode.make_image()
+        
+        #Save the image .png
+        path = './outputs/'+student+'/'+data+'.png'
+        QRimg.save(path)
+     
+file.close()
+
+
+
 
