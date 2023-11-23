d11dc0c052fdebe57a2bf55635f92aacaa25a7ee
Ronagar <rnginformatica@uma.es>
2023-11-23 11:26:21 +0100

    V3.Imagen incluida en QR(mejorar). Arreglada utf-8

--- createQR.py
+++ createQR.py
@@ -1,9 +1,17 @@
+# -*- coding: utf-8 -*-
 from pathlib import Path
 import qrcode
 from PIL import Image
 import csv 
 
-with open('alumnos.csv') as file:
+imagenlogo = './etsii.jpeg'
+logo = Image.open(imagenlogo)
+# Resize the image
+hsize = int((float(logo.size[1])*float(60/float(logo.size[0]))))
+#logo = logo.resize((100, hsize), Image.ANTIALIAS)
+logo = logo.resize((60, hsize))
+
+with open('alumnos.csv', encoding='utf-8') as file:
     reader = csv.DictReader(file)
     headers = [header.strip() for header in reader.fieldnames]
     for row in reader:
@@ -15,18 +23,19 @@ with open('alumnos.csv') as file:
         
 
         for letter in ('a','b','c'):
-            print(letter)
             QRcode = qrcode.QRCode(error_correction=qrcode.constants.ERROR_CORRECT_H)
             #extract the student id from the dataSet
             data = row.get(headers[2])
             #Feed the QR Code data
             data = data+'_'+letter
-            print(data)
             QRcode.add_data(data)
             QRcode.make
+            # Insert the image to the QR code and create the final image
+            QRimg = QRcode.make_image(fill_color='Black', back_color='White').convert('RGB')
 
-            #Creation of the image
-            QRimg = QRcode.make_image()
+            #We define the image position (center)
+            pos = ((QRimg.size[0] - logo.size[0]) // 2,(QRimg.size[1] - logo.size[1]) // 2)
+            QRimg.paste(logo, pos)
         
             #Save the image .png
             path = './outputs/'+student+'/'+data+'.png'
